783268167103514822978fd78badea9d34693eb4
Nicolas07n <nicolasmg4012@gmail.com>
2025-03-25 20:46:42 +0100

    Salto del pirata completado

--- Run Pirates/Assets/Scrips/Personaje/Pirata.cs
+++ Run Pirates/Assets/Scrips/Personaje/Pirata.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Pirata : MonoBehaviour
 {
     private Rigidbody2D pirataRb;
+    public float upForce = 5f;
     void Start()
     {
         pirataRb = GetComponent<Rigidbody2D>();
@@ -15,7 +17,7 @@ public class Pirata : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            pirataRb.AddForce(Vector2.up * upForce);
 
         }
     }
