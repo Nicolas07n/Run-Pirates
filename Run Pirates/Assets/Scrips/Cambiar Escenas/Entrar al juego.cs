31b9ccb22761d58f6ca72992bba022c574d1886c
Nicolas07n <nicolasmg4012@gmail.com>
2025-03-25 18:44:02 +0100

    Boton
    Es el boton de entrar al juego

--- Run Pirates/Assets/Scrips/Cambiar Escenas/Entrar al juego.cs
+++ Run Pirates/Assets/Scrips/Cambiar Escenas/Entrar al juego.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Entraraljuego : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Entrar()
     {
-        
-    }
+        SceneManager.LoadScene("SampleScene");//Con esto entras al juego
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
