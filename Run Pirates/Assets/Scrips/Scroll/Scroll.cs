5902279be95961d33cfdc58d76698de08e77866b
Nicolas07n <nicolasmg4012@gmail.com>
2025-03-26 17:40:20 +0100

    Scroll
    He creado un crip con fuerza cevtor 2 left . Hace que en cualquier objeto se mueva todo el tiempo hacia la izquierda

---
+++ Run Pirates/Assets/Scrips/Scroll/Scroll.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    private float scrollSpeed = 8f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left *  scrollSpeed * Time.deltaTime); //Hace que en todo lo que se lo pongas se mueva hacia la left izquierda.
+    }
+}
