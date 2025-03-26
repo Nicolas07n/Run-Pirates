90bde7ad7f604f598e4958b7f79f38d580d6c6bb
Nicolas07n <nicolasmg4012@gmail.com>
2025-03-26 17:14:53 +0100

    Ground Check
    He modificado el scrip de salto con overlapCircle que crea un circulo pequeño que cada vez que detecta el layer de ground salta ,

--- Run Pirates/Assets/Scrips/Personaje/Pirata.cs
+++ Run Pirates/Assets/Scrips/Personaje/Pirata.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class Pirata : MonoBehaviour
 {
     private Rigidbody2D pirataRb;
+    public Transform groundcheck; //La posiciendo de los pies del pirata
+    public LayerMask ground; // La capa del suelo
+    public float radius; //Radio del circulo , lo que hace es como crear un circulo pequeño en los pies que cada vez que toca el suelo detecta que es el suelo y puede saltar
+
     public float upForce = 5f;
     void Start()
     {
@@ -14,10 +18,17 @@ public class Pirata : MonoBehaviour
 
     
     void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+    { // El Overlap te crea el circulo invisible //posicion del punto  //el radio  //Y el layer
+        bool isGrounded = Physics2D.OverlapCircle(groundcheck.position , radius  ,  ground);//Si el circulo con el radio hace contacto con el ground devuelve true y toca suelo
+
+        if (Input.GetKeyDown(KeyCode.Space))// Salto 
         {
-            pirataRb.AddForce(Vector2.up * upForce);
+            if (isGrounded)//Si esta tocando el suelo hace el salto
+            {
+                pirataRb.AddForce(Vector2.up * upForce);
+            }
+
+            
 
         }
     }
